8f53261dd2d4eb0b6f438e8fcd4f35e174624987
kukovisuals <clubhousenet@live.com>
2022-06-29 12:12:12 -0400

    adding style in tables

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { Menu, Controller } from "../Components";
 import { widget } from "../data/widget";
-import axios from 'axios';
+import axios from "axios";
 
-    const url = 'https://random-data-api.com/api/users/random_user?size=100'
+const url = "https://random-data-api.com/api/users/random_user?size=100";
 
 function Dashboard() {
-    const [data, setData] = React.useState([])
-    const [load, setLoad] = React.useState(true)
-    const [, setError] = React.useState('')
-
+    const [data, setData] = React.useState([]);
+    const [load, setLoad] = React.useState(true);
+    const [, setError] = React.useState("");
 
     React.useEffect(() => {
         axios(url)
-            .then(res => setData(res.data))
-            .then(res => setLoad(false))
-            .catch(setError)
-    },[]) 
+            .then((res) => setData(res.data))
+            .then((res) => setLoad(false))
+            .catch(setError);
+    }, []);
 
     // data.map((d:any) => console.log(d.username) )
     return (
@@ -40,31 +39,99 @@ function Dashboard() {
                         />
                     ))}
                     <div className="dashboard-grid-table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>ORGANIZATION</th>
-                                <th>USERNAME</th>
-                                <th>EMAIL</th>
-                                <th>PHONE NUMBER</th>
-                                <th>DATE JOINED</th>
-                                <th>STATUS</th>
-                            </tr>
-                        </thead>
-                
-                    {!load && data.map( (d:any, i) =>  
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <div className="thead-img">
+                                        <span>
+                                            
+                                            ORGANIZATION
+                                        </span>
+                                        <img
+                                            alt="sort"
+                                            src="https://visualpharm.com/assets/35/Sorting%20Options-595b40b85ba036ed117de393.svg"
+                                        />
+                                        </div>
+                                    </th>
 
-                        <TableData
-                            key={d.email}
-                            organization="Lendsqr"
-                            username={d.username}
-                            email={d.email}
-                            phoneNumber={d.phone_number}
-                            dateJoined={d.date_of_birth}
-                            status={d.subscription.status}
-                        />
-                        ).filter( (d,i) => i < 10 )
-                    }
+                                    <th>
+                                        <div className="thead-img">
+                                        <span>
+                                            
+                                            USERNAME
+                                        </span>
+                                        <img
+                                            alt="sort"
+                                            src="https://visualpharm.com/assets/35/Sorting%20Options-595b40b85ba036ed117de393.svg"
+                                        />
+                                        </div>
+                                    </th>
+                                    <th>
+                                        <div className="thead-img">
+                                        <span>
+                                            
+                                            EMAIL
+                                        </span>
+                                        <img
+                                            alt="sort"
+                                            src="https://visualpharm.com/assets/35/Sorting%20Options-595b40b85ba036ed117de393.svg"
+                                        />
+                                        </div>
+                                    </th>
+                                    <th>
+                                        <div className="thead-img">
+                                        <span>
+                                            
+                                            PHONE NUMBER
+                                        </span>
+                                        <img
+                                            alt="sort"
+                                            src="https://visualpharm.com/assets/35/Sorting%20Options-595b40b85ba036ed117de393.svg"
+                                        />
+                                        </div>
+                                    </th>
+                                    <th>
+                                        <div className="thead-img">
+                                        <span>
+                                            
+                                            DATE JOINED
+                                        </span>
+                                        <img
+                                            alt="sort"
+                                            src="https://visualpharm.com/assets/35/Sorting%20Options-595b40b85ba036ed117de393.svg"
+                                        />
+                                        </div>
+                                    </th>
+                                    <th>
+                                        <div className="thead-img">
+                                        <span>
+                                            
+                                            STATUS
+                                        </span>
+                                        <img
+                                            alt="sort"
+                                            src="https://visualpharm.com/assets/35/Sorting%20Options-595b40b85ba036ed117de393.svg"
+                                        />
+                                        </div>
+                                    </th>
+                                </tr>
+                            </thead>
+
+                            {!load &&
+                                data
+                                    .map((d: any, i) => (
+                                        <TableData
+                                            key={d.email}
+                                            organization="Lendsqr"
+                                            username={d.username}
+                                            email={d.email}
+                                            phoneNumber={d.phone_number}
+                                            dateJoined={d.date_of_birth}
+                                            status={d.subscription.status}
+                                        />
+                                    ))
+                                    .filter((d, i) => i < 10)}
                         </table>
                     </div>
 
@@ -110,13 +177,13 @@ function Dashboard() {
     );
 }
 
-interface TableProps{
+interface TableProps {
     organization: string;
     username: string;
     email: string;
     phoneNumber: string;
     dateJoined: string;
-    status:string;
+    status: string;
 }
 
 interface DisplayProps {
@@ -127,9 +194,8 @@ interface DisplayProps {
 }
 
 const TableData = (props: TableProps) => {
-
-    if(props.username === ''){
-        return <pre>loading</pre>
+    if (props.username === "") {
+        return <pre>loading</pre>;
     }
     return (
         <tbody>
@@ -142,18 +208,21 @@ const TableData = (props: TableProps) => {
                 <td>{props.status}</td>
             </tr>
         </tbody>
-    )
-}
+    );
+};
 
 function DisplayTypes(props: DisplayProps) {
     const arrColors = ["aqua", "bisque", "coral", "violet"];
 
-    const colorId = arrColors[props.id - 1]
+    const colorId = arrColors[props.id - 1];
 
     return (
         <div className="dashboard-grid-types">
             <div className="dashboard-grid-types-container">
-                <div className="dashboard-grid-types-image" style={{'backgroundColor': colorId}}>
+                <div
+                    className="dashboard-grid-types-image"
+                    style={{ backgroundColor: colorId }}
+                >
                     <img alt="user profile" src={props.icon} />
                 </div>
                 <div className="dashboard-grid-types-name">
